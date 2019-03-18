65de9ece2b2b0c9d516f1c4c7fd72f89817e183a
DDDavid97 <sustc1526fan@hotmail.com>
2019-03-18 21:06:42 +0800

    upload src code

---
+++ ideaj.example/src/main/java/codeToTest.java
@@ -0,0 +1,9 @@
+public class codeToTest {
+    public static int compare(int a, int b){
+        if(a > b){
+            return a;
+        }else{
+            return b;
+        }
+    }
+}
---
+++ ideaj.example/src/test/java/codeToTestTest.java
@@ -0,0 +1,13 @@
+import static org.junit.Assert.*;
+import org.junit.Test;
+public class codeToTestTest {
+    @Test
+    public void test(){
+        codeToTest.compare(1, 2);
+    }
+
+    @Test
+    public void test0(){
+        codeToTest.compare(3, 2);
+    }
+}
